aeccecff91b295feeb885aa23569033acfc55b81
MIYABETaisei <mytspe46@gmail.com>
2021-03-13 17:02:53 +0900

    課題の回答を更新

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    can_drink = amount
+    count = amount
+    while count >= 3
+      get_drink = count / 3
+      em_drink = count % 3
+      can_drink += get_drink
+      count = get_drink + em_drink
+    end
+    return can_drink
   end
 end
+
+p BonusDrink.total_count_for(90)
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -2,4 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it '0個購入したとき0本飲める' do
+    expect(BonusDrink.total_count_for(0)).to eq 0
+  end
+  it '1個購入したとき1本飲める' do
+    expect(BonusDrink.total_count_for(1)).to eq 1
+  end
+  it '3個購入したとき4本飲める' do
+    expect(BonusDrink.total_count_for(3)).to eq 4
+  end
+  it '11個購入したとき16本飲める' do
+    expect(BonusDrink.total_count_for(11)).to eq 16
+  end
+  it '100個購入したとき149本飲める' do
+    expect(BonusDrink.total_count_for(100)).to eq 149
+  end
 end
